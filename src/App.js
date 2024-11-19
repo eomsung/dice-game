239510a2fb6d1add0db11855837fdd24adcf541b
sungmin <usm0259@kaist.ac.kr>
2024-11-19 17:44:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button } from './Button';
+import { Dice } from './Dice';
+
+const DEFAULT_NUM =6
+
+const random =(n)=>{
+  return Math.floor(Math.random() * n)+1
+}
 
 function App() {
+  const [num, setNum] = useState(DEFAULT_NUM)
+  const [gameHistory, setGameHistory] = useState([])
+  const handleRollClick = () => {
+    const random_num = random(6)
+    setNum(random_num)
+    gameHistory.push(random_num)
+    setGameHistory([...gameHistory])
+  }
+  const handleResetClick =() =>{
+    setNum(DEFAULT_NUM);
+    setGameHistory([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          <Dice color="blue" num={num}/>
+          <Button onClick={handleRollClick}>던지기</Button>
+          <Button onClick={handleResetClick}>처음부터</Button>
+          <div>
+            {gameHistory.join(',')}
+          </div>
+    </>
   );
 }
 
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+export function Button(props){
+
+    return(
+        <button onClick={props.onClick}>
+            {props.children}
+            </button>
+    )
+}
